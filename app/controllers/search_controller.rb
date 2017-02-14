12ccd11e900a08748ebae576d9dcf34d0179df82
dubeboy <216004854@student.uj.ac.za>
2017-02-14 19:51:28 +0200

    fix typo in new book

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
-    @cat = Category.all
+    @cat = Category.all.select { |x| x if x.book_type == "Book"}
     if params[:category] == "1"
         @acs = Accommodation.msearch(params[:search]).paginate(page: params[:page], per_page: 16).order(created_at: :desc)
     else
