4af1c811784d0027b29a0e4b5d7c820fb90b94f8
dubeboy <216004854@student.uj.ac.za>
2018-09-20 00:11:36 +0200

    ...
    some really deadly stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,15 +86,15 @@ Rails.application.configure do
 
    ######PAPERCLIP CONFIGS#######
   config.paperclip_defaults = {
-  storage: :s3,
+  storage: :s4,
   s3_credentials: {
-    bucket: 'divinesbucket',
-    access_key_id: 'AKIAIJBINCJK4SCVIBUA',
-    secret_access_key: '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',
-    s3_region: 'Oregon',
+    bucket: 'kiki',
+    access_key_id: 'kkiki',
+    secret_access_key: 'kkiikkii+s4yRQapovbCPp9',
+    s3_region: 'kikiiki',
    },
-    :url =>':s3_domain_url',
-    :path => '/:class/:attachment/:id_partition/:style/:filename',
+    :url =>':deeeee',
+    :path => 'eeee/:style/:filename',
   }
   
 end
